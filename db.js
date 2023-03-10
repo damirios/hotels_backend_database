1f0a9ffac2692000330a8d32e2d0084c5d2a8a7c
damirios <damirdigaz@gmail.com>
2023-03-10 10:29:13 +0300

    default db password added

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const Pool = require('pg').Pool;
 const pool = new Pool({
     user: process.env.DB_USER || "postgres",
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || 'root',
     host: 'localhost',
     port: 5432,
     database: process.env.DB || 'hotels_film_db_simplified'
